f55aab973e5da7d9ecacfefb8b605f0073753cfa
lilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-05-19 13:26:52 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='skroutz',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Lil Thanasis',
+    author_email='mitthan87@gmail.com',
+    description=''
+)
